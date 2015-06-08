7a83e87bbd0a1a80dbb8595b3d24037e8bae72f5
Carl Waldbieser <cwaldbieser@gmail.com>
2015-06-07 22:10:48 -0400

    Colorized manhole integration.

--- txsshadmin/ssh_service.py
+++ txsshadmin/ssh_service.py
@@ -2,14 +2,18 @@
 
 from twisted.cred.portal import Portal
 from twisted.cred.checkers import FilePasswordDB
+from twisted.conch import manhole_ssh
 from twisted.conch.checkers import (
     SSHPublicKeyChecker,
     UNIXAuthorizedKeysFiles)
 from twisted.conch.ssh.factory import SSHFactory
 from twisted.internet import reactor
 from twisted.conch.ssh.keys import Key
+from twisted.conch.insults import insults
 from twisted.conch.interfaces import IConchUser
 from twisted.conch.avatar import ConchUser
+from twisted.conch.manhole import ColoredManhole
+from twisted.conch.ssh import session
 from twisted.conch.ssh.channel import SSHChannel
 from twisted.python import log
 import sys
@@ -27,31 +31,45 @@ with open('keys/id_rsa.pub') as publicBlobFile:
 def nothing():
     pass
 
-class SimpleSession(SSHChannel):
-    name = 'session'
+#class SimpleSession(SSHChannel):
+#    name = 'session'
+#
+#    def request_pty_req(self, data):
+#        return True
+#
+#    def request_shell(self, data):
+#        return True
+#
+#    def dataReceived(self, bytes):
+#        self.write("echo: " + repr(bytes) + "\r\n")
 
-    def request_pty_req(self, data):
-        return True
+#class SimpleRealm(object):
+#    def requestAvatar(self, avatarId, mind, *interfaces):
+#        user = ConchUser()
+#        user.channelLookup['session'] = SimpleSession
+#        return IConchUser, user, nothing
 
-    def request_shell(self, data):
-        return True
-
-    def dataReceived(self, bytes):
-        self.write("echo: " + repr(bytes) + "\r\n")
-
-class SimpleRealm(object):
-    def requestAvatar(self, avatarId, mind, *interfaces):
-        user = ConchUser()
-        user.channelLookup['session'] = SimpleSession
-        return IConchUser, user, nothing
+class chainedProtocolFactory:
+    def __init__(self, namespace):
+        self.namespace = namespace
+     
+    def __call__(self):
+        return insults.ServerProtocol(ColoredManhole, self.namespace)
 
 factory = SSHFactory()
 factory.privateKeys = {'ssh-rsa': privateKey}
 factory.publicKeys = {'ssh-rsa': publicKey}
-factory.portal = Portal(SimpleRealm())
+sshRealm = manhole_ssh.TerminalRealm()
+namespace = {
+        'foo': 'baz',
+        'lst': [0, 1, 2],
+        }
+sshRealm.chainedProtocolFactory = chainedProtocolFactory(namespace)
+sshPortal = Portal(sshRealm)
+factory.portal = sshPortal
 #factory.portal.registerChecker(FilePasswordDB("ssh-passwords"))
 keydb = UNIXAuthorizedKeysFiles()
 factory.portal.registerChecker(SSHPublicKeyChecker(keydb))
-
 reactor.listenTCP(2022, factory)
 reactor.run()
+
